1d6d061a30fc90c8782032c169926982e34df3be
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2025-01-23 15:47:11 -0300

    refactor(structure): refactor file structure

---
+++ src/Edge.php
@@ -0,0 +1,35 @@
+<?php
+namespace App;
+use App\Vertex;
+
+class Edge {
+    private Vertex $source;
+    private Vertex $target;
+
+    public function __construct(Vertex $source, Vertex $target) {
+        $this->source = $source;
+        $this->target = $target;
+    }
+
+    public function getVertices() {
+        return [$this->source, $this->target];
+    }
+
+    public function getVerticesIds() {
+        return [$this->source->getId(), $this->target->getId()];
+    }
+
+    public function setVertices(Vertex $source, Vertex $target) {
+        $this->source = $source;
+        $this->target = $target;
+    }
+
+    public function contains(Vertex $vertex) {
+        return $this->source === $vertex || $this->target === $vertex;
+    }
+
+    public function show() {
+        return "{".$this->source->getID().", ".$this->target->getId()."}";
+    }
+}
+?>
---
+++ src/Vertex.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+class Vertex {
+    private $id;
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $thid->id = $id;
+    }
+}
+?>
