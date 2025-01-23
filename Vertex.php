c230a398aef0b3b52cab50e34078e00edf62d91f
Vitor Almeida Subhi <vitorsubhi@gmail.com>
2025-01-23 14:27:55 -0300

    feat(Vertex): add Vertex class

---
+++ Vertex.php
@@ -0,0 +1,17 @@
+<?php
+    class Vertex {
+        private $id;
+
+        public function __construct($id) {
+            $this->id = $id;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function setId($id){
+            $thid->id = $id;
+        }
+    }
+?>
